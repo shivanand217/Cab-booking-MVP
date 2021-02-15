2a8830ed712c1754f618b66a756c854ab08214a0
Shiv <shivakp2111@gmail.com>
2021-02-15 11:53:03 +0530

    cleanups

--- src/main/java/com/uber/uberapi/controllers/DemoController.java
+++ src/main/java/com/uber/uberapi/controllers/DemoController.java
@@ -8,25 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-@Builder
-public class DemoController implements CommandLineRunner {
-    @Autowired
-    DriverRepository driverRepository;
-
-    @Autowired
-    AccountRepository accountRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Demo controller running...");
-
-        Account account1 = Account.builder()
-                                .username("ajay_dspod")
-                                .password("123")
-                                .build();
-        System.out.println("account id is " + account1.getId());
-        accountRepository.save(account1);
-        System.out.println("account id is " + account1.getId());
-    }
-}
+//@Component
+//@Builder
+//public class DemoController implements CommandLineRunner {
+//    @Autowired
+//    DriverRepository driverRepository;
+//
+//    @Autowired
+//    AccountRepository accountRepository;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("Demo controller running...");
+//
+//        Account account1 = Account.builder()
+//                                .username("ajay_dspod")
+//                                .password("123")
+//                                .build();
+//        System.out.println("account id is " + account1.getId());
+//        accountRepository.save(account1);
+//        System.out.println("account id is " + account1.getId());
+//    }
+//}
